50ea44f7d26bc9432fb5a0637e496479e3ba01c9
lab314-21@labeng.local <lab314-21@labeng.local>
2019-03-01 15:52:41 +0700

    1

---
+++ pracex2_4.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double sg(double , double );
+
+
+int main(){
+    cout << sg(-2.5,1.1) << "\n";
+    cout << sg(1,10000) << "\n";
+    cout << sg(8.5,2.4) << "\n";
+    cout << sg(0.1,0.0001) << "\n";
+    cout << sg(10,0.007) << "\n";
+    cout << sg(1,-2) << "\n";
+    cout << sg(15,100) << "\n";
+    cout << sg(123,5432) << "\n";
+}
+
+double sg(double x, double y){
+	if(x<=0 or y<=0) return 0;
+	if((x/y<=10 && x>y) or (y/x<=10 && y>x)) return pow(x*y,0.5);
+	else return sg(x,pow(x*y,0.5))+sg(y,pow(x*y,0.5));
+}
