d56e28fb59cddff67014f677506af90b8c6b250f
lab314-21@labeng.local <lab314-21@labeng.local>
2019-03-01 15:53:42 +0700

    1

---
+++ pracex2_3.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+char table[8][8];
+
+void showTable(char);
+void randomTable(char);
+
+int main(){
+	srand(time(0));	
+	randomTable(table);
+	showTable(table);
+}
+
+void showTable(const char table[][8]){
+	for(int i = 0; i < 8; i++){
+		for(int j = 0; j < 8; j++){
+			cout << table[i][j] << " ";
+		}
+		cout << "\n";
+	}
+}
+void randomTable(char table[][8]){
+	for(int i=0;i<8;i++){
+		for(int j=0;j<8;j++){	
+			table[i][j]=rand()%26-65;
+		}
+	}
+	
+}
+
+
